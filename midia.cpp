03aa22ac5659fb4431b31872ccb36192c55f76b5
Cecote <thiago.cecote@aluno.ufop.edu.br>
2022-05-15 13:16:26 -0300

    musica e filme

--- midia.cpp
+++ midia.cpp
@@ -1,9 +1,43 @@
 #include <iostream>
+#include <string>
 #include <vector>
-#include <io
 using namespace std;
 
-class Midia{
-    //artistas
-    string 
-}
+class Midia {
+    // artistas
+    string titulo;
+    int data;
+    int duracao;
+    // keywords
+    int numeroAcessos;
+
+   public:
+    // construtores????????
+
+    // set get artistas
+    string getTitulo() {
+        return titulo;
+    }
+    void setTitulo(string tituloNovo) {
+        this->titulo = tituloNovo;
+    }
+    int getData() {
+        return data;
+    }
+    void setData(int dataNova) {
+        this->data = dataNova;
+    }
+    int getDuracao() {
+        return duracao;
+    }
+    void setDuracao(int duracaoNova) {
+        this->duracao = duracaoNova;
+    }
+    // getsetkeywords
+    int getNumeroAcessos() {
+        return numeroAcessos;
+    }
+    void setNumeroAcessos(int numeroAcessosNovo) {
+        this->numeroAcessos = numeroAcessosNovo;
+    }
+};
---
+++ musica.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "midia.cpp"
+using namespace std;
+
+class Musica : public Midia {
+    // formatos de audio
+    // generos de musica
+};
