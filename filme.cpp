5b491e134f7e85d8f6319c9485c26aec65567db0
lsclgr <calegariluisa@gmail.com>
2022-05-15 14:38:32 -0300

    filme

---
+++ filme.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "midia.cpp"
+using namespace std;
+
+class Filme : public Midia {
+    // formatos de video
+    // generos de filme
+};
